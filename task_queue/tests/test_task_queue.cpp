b23657dadd8fc833b797d240b4e6f9e2bad1f503
xiesongde <726982597@qq.com>
2025-09-07 16:09:29 +0800

    [modify] add module test

---
+++ task_queue/tests/test_task_queue.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "utoolkit/task_queue/task_queue.h"
+
+TEST(TaskQueueTest, BasicTest) {
+    // 基本测试占位符
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
